78d9e7b4e55523e6586e208604ff587f7bce34b8
Daniel Chu <dchu@ualberta.ca>
2020-06-03 15:31:10 -0600

    added output and ordering

--- python/FrequencyFunctionValueGenerate.py
+++ python/FrequencyFunctionValueGenerate.py
@@ -20,8 +20,30 @@ for i in range(14):
     tts.append(SContainer(fcn(i,j),i,j))
 
 tts.sort(key=lambda x:x.val,reverse=True)
-
+tts.reverse()
 for stuff in tts:
   print("s{1} r{2} \n   {0:.0f}".format(stuff.val,stuff.s,stuff.r))
 
 print("\n\n\n\nSIZE {}".format(len(tts)))
+
+opstring15 = ";15 Step\ndb"
+opstring7 = ";7 Step\ndb"
+
+for entry in tts:
+
+  temp = entry.s << 4
+  temp = temp | entry.r
+  opstring15 += " ${}, ".format(hex(temp)[2:]) 
+  temp = temp | 8
+  opstring7 += " ${}, ".format(hex(temp)[2:]) 
+
+  opstring15 = opstring15
+  opstring7 = opstring7
+
+
+f = open("noiseTable.asm","w")
+f.write(opstring15[:-2])
+f.write("\n")
+f.write(opstring7[:-2])
+f.close()
+
